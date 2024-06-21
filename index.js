c7e5a99c025f6839bdc76a7bb43c9ea237a4d59b
Prabal Arora <prabalarora2001@gmail.com>
2024-06-21 15:02:09 +0530

    env hide

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const fs = require("fs/promises");
 const axios = require("axios");
+require("dotenv").config();
 const app = express();
 
 const PORT = process.env.PORT || 3000;
